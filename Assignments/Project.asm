0c29e1df2fcc101659ee003e086b803759d72279
Corbin Matamoros <35641464+Pirhomega@users.noreply.github.com>
2018-12-07 15:21:57 -0600

    Update Project.asm

--- Assignments/Project.asm
+++ Assignments/Project.asm
@@ -34,7 +34,7 @@ NOP               ;(90)
 
 ;put N = 2 into memory at address 106 (02)
 
-MOV	    AX, [106]     ;(8B 06 06 01)
-ADD     AX, 346       ;(81 C0 5A 01)
-MOV    	BL, 3         ;(C6 C3 03)
-DIV     BL            ;(F6 F3)
+MOV	        AX, [106]     ;(8B 06 06 01)
+ADD    AX, 346       ;(81 C0 5A 01)
+MOV    	    BL, 3         ;(C6 C3 03)
+DIV         BL            ;(F6 F3)
